a1b8a6fc07572c34bafc23998fa4357feedaeea0
Max Baun <max@d3applications.com>
2018-10-18 19:31:00 -0400

    faq section

--- src/components/page/page.js
+++ src/components/page/page.js
@@ -7,6 +7,7 @@ import SectionHero from '../sections/sectionHero';
 import SectionAbout from '../sections/sectionAbout';
 import SectionVideos from '../sections/sectionVideos';
 import SectionTeam from '../sections/sectionTeam';
+import SectionFaq from '../sections/sectionFaq';
 
 import HeroImg from '../../img/Hero-bg-1.png';
 import AboutBg from '../../img/About-Bg.jpg';
@@ -407,6 +408,43 @@ const SectionTeamMembers = fromJS([
 	}
 ]);
 
+const FaqHeading = `# Frequently
+# Asked <span style="color:#0091D6;">Questions</span>`;
+
+const Faqs = fromJS([
+	{
+		question: 'How much does it cost?',
+		answer: `It costs....
+
+		alot....`
+	},
+	{
+		question: 'What schools and artists have you worked with?',
+		answer: `This is the answer to the question highlighted on the left. This will dynamically change based on the question selected. Think of this element as vertical tabs. Payoff bankroll convention center vdara sin city flush; chips flamingo hotel town square treasure island?
+		Chateau the pairs royal flush baccarat the excalibur swim-up blackjack sin city? All in chateau the venetian swim-up blackjack veer towers flush, draw fruit loop odds fremont street! Haze two pair exposure all in nightclubs caesar's palace zappos. Golden nugget vegas givecamp margaritaville loose parlay, push city center tease chateau. Capriotti's miracle mile shops feeling lucky town square the strip the excalibur dealer!`
+	},
+	{
+		question: 'Wait... are you a DJ company?',
+		answer: `This is the answer to the question highlighted on the left. This will dynamically change based on the question selected. Think of this element as vertical tabs. Payoff bankroll convention center vdara sin city flush; chips flamingo hotel town square treasure island?
+		Chateau the pairs royal flush baccarat the excalibur swim-up blackjack sin city? All in chateau the venetian swim-up blackjack veer towers flush, draw fruit loop odds fremont street! Haze two pair exposure all in nightclubs caesar's palace zappos. Golden nugget vegas givecamp margaritaville loose parlay, push city center tease chateau. Capriotti's miracle mile shops feeling lucky town square the strip the excalibur dealer!`
+	},
+	{
+		question: 'I already have local vendor which I like. Can we involve them in my event?',
+		answer: `This is the answer to the question highlighted on the left. This will dynamically change based on the question selected. Think of this element as vertical tabs. Payoff bankroll convention center vdara sin city flush; chips flamingo hotel town square treasure island?
+		Chateau the pairs royal flush baccarat the excalibur swim-up blackjack sin city? All in chateau the venetian swim-up blackjack veer towers flush, draw fruit loop odds fremont street! Haze two pair exposure all in nightclubs caesar's palace zappos. Golden nugget vegas givecamp margaritaville loose parlay, push city center tease chateau. Capriotti's miracle mile shops feeling lucky town square the strip the excalibur dealer!`
+	},
+	{
+		question: 'What sort of equipment do you have?',
+		answer: `This is the answer to the question highlighted on the left. This will dynamically change based on the question selected. Think of this element as vertical tabs. Payoff bankroll convention center vdara sin city flush; chips flamingo hotel town square treasure island?
+		Chateau the pairs royal flush baccarat the excalibur swim-up blackjack sin city? All in chateau the venetian swim-up blackjack veer towers flush, draw fruit loop odds fremont street! Haze two pair exposure all in nightclubs caesar's palace zappos. Golden nugget vegas givecamp margaritaville loose parlay, push city center tease chateau. Capriotti's miracle mile shops feeling lucky town square the strip the excalibur dealer!`
+	},
+	{
+		question: 'How do I know if it\'s a good fit',
+		answer: `This is the answer to the question highlighted on the left. This will dynamically change based on the question selected. Think of this element as vertical tabs. Payoff bankroll convention center vdara sin city flush; chips flamingo hotel town square treasure island?
+		Chateau the pairs royal flush baccarat the excalibur swim-up blackjack sin city? All in chateau the venetian swim-up blackjack veer towers flush, draw fruit loop odds fremont street! Haze two pair exposure all in nightclubs caesar's palace zappos. Golden nugget vegas givecamp margaritaville loose parlay, push city center tease chateau. Capriotti's miracle mile shops feeling lucky town square the strip the excalibur dealer!`
+	}
+]);
+
 export default class Page extends Component {
 	constructor(props) {
 		super(props);
@@ -486,6 +524,7 @@ export default class Page extends Component {
 					state={this.props.state}
 					team={SectionTeamMembers}
 				/>
+				<SectionFaq backgroundImage={fromJS({src: AboutBg})} heading={FaqHeading} faqs={Faqs} state={this.props.state} allFaqLink="/"/>
 			</div>
 		);
 	}
---
+++ src/components/sections/sectionFaq.js
@@ -0,0 +1,127 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import * as ImmutableProptypes from 'react-immutable-proptypes';
+import {List, Map} from 'immutable';
+import {Link} from 'react-router-dom';
+
+import CSS from './sectionFaq.module.scss';
+import HeadingBrand from '../headingBrand/headingBrand';
+import Markdown from '../common/markdown';
+import {click} from '../../utils/componentHelpers';
+import {responsive} from '../../constants';
+
+export default class SectionFaq extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			activeIndex: 0
+		};
+
+		this.handleFaqChange = this.handleFaqChange.bind(this);
+	}
+
+	static propTypes = {
+		heading: PropTypes.string,
+		faqs: ImmutableProptypes.list,
+		backgroundImage: ImmutableProptypes.map,
+		state: ImmutableProptypes.map,
+		allFaqLink: PropTypes.string
+	};
+
+	static defaultProps = {
+		heading: '',
+		faqs: List(),
+		backgroundImage: Map(),
+		state: Map(),
+		allFaqLink: '/'
+	};
+
+	handleFaqChange(activeIndex) {
+		this.setState({activeIndex});
+	}
+
+	render() {
+		const windowWidth = this.props.state.getIn(['windowSize', 'width']);
+		const backgroundStyle = {
+			backgroundImage: `url(${this.props.backgroundImage.get('src')})`
+		};
+
+		return (
+			<div className={CSS.section}>
+				<div className={CSS.overlay}/>
+				<div className={CSS.background} style={backgroundStyle}/>
+				<div className={CSS.inner}>
+					<div className={CSS.heading}>
+						<HeadingBrand heading={this.props.heading}/>
+					</div>
+					{windowWidth < responsive.collapse ? this.renderAccordionTabs() : this.renderVerticalTabs()}
+					<div className={CSS.allFaqLink}>
+						<Link to={this.props.allFaqLink} className={CSS.allLink}>
+							View all questions
+						</Link>
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+	renderAccordionTabs() {
+		return (
+			<div className={CSS.accordion}>
+				<ul className={CSS.accordionList}>
+					{this.props.faqs.map((faq, index) => {
+						const itemCss = [CSS.accordionItem];
+						const questionCss = [CSS.question];
+
+						if (index === this.state.activeIndex) {
+							itemCss.push(CSS.accordionItemActive);
+							questionCss.push(CSS.questionActive);
+						}
+						return (
+							<li key={faq.get('question')} className={itemCss.join(' ')}>
+								<div className={questionCss.join(' ')} onClick={click(this.handleFaqChange, index)}>
+									<span>{faq.get('question')}</span>
+								</div>
+								<div className={CSS.answer}>
+									<Markdown content={faq.get('answer')}/>
+								</div>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		);
+	}
+
+	renderVerticalTabs() {
+		return (
+			<div className={CSS.row}>
+				<div className={CSS.col}>
+					<div className={CSS.questions}>
+						<ul className={CSS.questionList}>
+							{this.props.faqs.map((faq, index) => {
+								const questionCss = [CSS.question];
+
+								if (index === this.state.activeIndex) {
+									questionCss.push(CSS.questionActive);
+								}
+
+								return (
+									<li key={faq.get('question')} className={questionCss.join(' ')} onClick={click(this.handleFaqChange, index)}>
+										<span>{faq.get('question')}</span>
+									</li>
+								);
+							})}
+						</ul>
+					</div>
+				</div>
+				<div className={CSS.col}>
+					<div className={CSS.answer}>
+						<Markdown content={this.props.faqs.getIn([this.state.activeIndex, 'answer'])}/>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
